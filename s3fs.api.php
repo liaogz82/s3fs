f601e097cb2b8d7d17625fb5ac8e861e852171a4
nachosalvador <nachosalvador@2927399.no-reply.drupal.org>
2017-04-03 20:38:13 +0200

    Issue #2863732 by nachosalvador: Coding standards s3fs.api.php

--- s3fs.api.php
+++ s3fs.api.php
@@ -2,9 +2,10 @@
 
 /**
  * @file
- * This file contains no working PHP code; it exists to provide additional
- * documentation for doxygen as well as to document hooks in the standard
- * Drupal manner.
+ * This file contains no working PHP code.
+ *
+ * It exists to provide additional documentation for doxygen as well as to
+ * document hooks in the standard Drupal manner.
  */
 
 /**
@@ -30,17 +31,13 @@
  *       your own set of custom GET arguments to the S3 URLs of your files.
  *       If your custom args' keys start with "x-", S3 will ignore them, but
  *       still log them:
- *       http://docs.aws.amazon.com/AmazonS3/latest/dev/LogFormat.html#LogFormatCustom
- *
+ *       http://docs.aws.amazon.com/AmazonS3/latest/dev/LogFormat.html#LogFormatCustom.
  * @param string $s3_file_path
  *   The path to the file within your S3 bucket. This includes the prefixes
  *   which might be added (e.g. s3fs-public/ for public:// files, or the
  *   S3FS Root Folder setting).
- *
- * @return array
- *   The modified array of configuration items.
  */
-function hook_s3fs_url_settings_alter(&$url_settings, $s3_file_path) {
+function hook_s3fs_url_settings_alter(array &$url_settings, $s3_file_path) {
   // An example of what you might want to do with this hook.
   if ($s3_file_path == 'myfile.jpg') {
     $url_settings['presigned_url'] = TRUE;
@@ -57,10 +54,11 @@ function hook_s3fs_url_settings_alter(&$url_settings, $s3_file_path) {
  * Alters the S3 file parameters when uploading an object.
  *
  * @param array $upload_params
- *   Associative array of upload settings
+ *   Associative array of upload settings.
+ *
  * @see http://docs.aws.amazon.com/aws-sdk-php/latest/class-Aws.S3.S3Client.html#_putObject
-*/
-function hook_s3fs_upload_params_alter(&$upload_params) {
+ */
+function hook_s3fs_upload_params_alter(array &$upload_params) {
   if (strpos($upload_params['Key'], 'private/') !== FALSE) {
     $upload_params['ACL'] = 'private';
   }
